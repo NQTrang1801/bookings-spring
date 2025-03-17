8964fc381bfa190424f37e6e1420bb6fd0024e0d
NQTrang1801 <trangnguyen180103@gmail.com>
2025-03-18 00:02:04 +0700

    init

---
+++ air-service/src/test/java/com/bm/airservice/AirServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm.airservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
