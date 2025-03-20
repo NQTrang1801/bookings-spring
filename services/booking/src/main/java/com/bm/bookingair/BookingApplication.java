dcf326ba62a65f1f04e21e8cd2c9edeaf49e457b
NQTrang1801 <trangnguyen180103@gmail.com>
2025-03-20 21:27:47 +0700

    add first unervisioned files

---
+++ services/booking/src/main/java/com/bm/bookingair/BookingApplication.java
@@ -0,0 +1,13 @@
+package com.bm.bookingair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingApplication.class, args);
+    }
+
+}
